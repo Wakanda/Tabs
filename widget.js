5a71e9236f0008bcf20e6e4d6c507f0abcdfd5e0
Stéphane Puybareau <stephane.puybareau@4d.com>
2014-07-24 14:16:16 +0200

    First commit

---
+++ widget.js
@@ -0,0 +1,113 @@
+WAF.define('Tabs', ['waf-core/widget', 'TabsBar', 'TabsContainer'], function(widget, TabsBar, TabsContainer) {
+    "use strict";
+
+    var Tabs = widget.create('Tabs');
+    Tabs.inherit('waf-behavior/layout/multicontainer');
+    Tabs.inherit('waf-behavior/layout/composed');
+    Tabs.inherit('waf-behavior/layout/properties-container');
+
+    Tabs.addProperty('menuPosition', {
+        type: 'enum',
+        values: {
+            'waf-tabview2-topLeft':     'top-left',
+            'waf-tabview2-topRight':    'top-right',
+            'waf-tabview2-leftTop':     'left-top',
+            'waf-tabview2-leftBottom':  'left-bottom',
+            'waf-tabview2-bottomLeft':  'bottom-left',
+            'waf-tabview2-bottomRight': 'bottom-right',
+            'waf-tabview2-rightTop':    'right-top',
+            'waf-tabview2-rightBottom': 'right-bottom'
+        },
+        onChange: function(value, oldValue) {
+            this.removeClass(oldValue);
+            this.addClass(value);
+        },
+        defaultValueCallback: function() {
+            var m = /waf-tabview2-(top|left|bottom|right)(Left|Right|Top|bottom)/.exec(this.node.className);
+            if(m) {
+                return m[0];
+            }
+            return 'waf-tabview2-topLeft';
+        }
+    });
+
+    Tabs.setPart('menubar', TabsBar);
+
+    Tabs.addProperty('tabs', {
+        type: 'list',
+        attributes: [{
+            name: 'title',
+            type: 'string',
+            defaultValueCallback: function() {
+                return 'Tab ' + (this.tabs.count() + 1);
+            }
+        }, {
+            name: 'closeButton',
+            type: 'boolean'
+        }]
+    });
+    Tabs.linkListPropertyToContainer('tabs');
+
+    Tabs.prototype.init = function() {
+        // force the default position css class
+        this.addClass(this.menuPosition());
+
+        var menubar = this.getPart('menubar');
+        this._menubarSelectSubscriber = menubar.subscribe('select', function(event) {
+            this.currentContainerIndex(event.data.index);
+        }, this);
+
+        menubar.subscribe('close', function(event) {
+            this.removeContainer(event.data.index);
+        }, this);
+
+        //synhronise tabs with items
+        this.tabs.onInsert(function(data) {
+            var item = this.tabs(data.index);
+            menubar.items.insert(data.index, {
+                value: item.title,
+                closeButton: item.closeButton
+            });
+            if(menubar.items.count() === 1) {
+                this.currentContainerIndex(0);
+            }
+        });
+        this.tabs.onRemove(function(data) {
+            menubar.items.remove(data.index);
+            if(menubar.items.count() && this.currentContainerIndex() === data.index) {
+                var index = data.index;
+                if(index >= menubar.items.count()) {
+                    index--;
+                }
+                this.currentContainerIndex(index);
+            }
+        });
+        this.tabs.onMove(function(data) {
+            menubar.items.move(data.from, data.to);
+        });
+        this.tabs.onModify(function(data) {
+            var item = this.tabs(data.index);
+            menubar.items(data.index, {
+                value: item.title,
+                closeButton: item.closeButton
+            });
+        });
+
+        if (this.countContainers() > 0 && this.currentContainerIndex() === undefined) {
+            this.currentContainerIndex(0);
+        }
+    };
+
+    Tabs.doAfter('currentContainerIndex', function(index) {
+        if(arguments.length) {
+            this._menubarSelectSubscriber.pause();
+            this.getPart('menubar').select(index);
+            this._menubarSelectSubscriber.resume();
+        }
+    });
+
+    Tabs.defaultContainer(TabsContainer);
+    Tabs.restrictWidget(TabsContainer);
+
+    return Tabs;
+});
